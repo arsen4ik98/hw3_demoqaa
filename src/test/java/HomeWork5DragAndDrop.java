0d18e9f4d4d37199d16a91759b04667dae821728
Arsen <arsenbeglaryan98@gmail.com>
2025-01-23 13:09:44 +0300

    Добавил allure

--- src/test/java/HomeWork5DragAndDrop.java
+++ src/test/java/HomeWork5DragAndDrop.java
@@ -22,6 +22,7 @@ import static com.codeborne.selenide.Selenide.$;
          Configuration.browserSize = "1920x1080";
          Configuration.baseUrl = "https://the-internet.herokuapp.com/";
          Configuration.pageLoadStrategy = "eager";
+         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
      }
 
 
