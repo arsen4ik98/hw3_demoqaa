d565b9ce9a64ca84dc920d17efd3f6b5284ae809
Arsen <arsenbeglaryan98@gmail.com>
2025-01-27 13:48:21 +0300

    Сделал домашку 12 и 13

--- src/test/java/HomeWork4SelenideTest.java
+++ src/test/java/HomeWork4SelenideTest.java
@@ -24,9 +24,11 @@ public class HomeWork4SelenideTest {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("windowSize", "1920x1080");
         Configuration.baseUrl = "https://github.com/"; // practiceFormTest
+        String remoteUrl = System.getProperty("remoteBrowserUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = config.remote();
+        //Configuration.remote = config.remote();
+        Configuration.remote = remoteUrl;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map .<String, Object>of(
                 "enableVNC", true,
